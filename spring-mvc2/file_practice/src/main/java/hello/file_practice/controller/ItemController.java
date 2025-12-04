fcb816e27ecf9dc81b086784ac40c6290a5737b7
김흥석 <eovnfjfpa@naver.com>
2025-12-04 23:15:44 +0900

    feat: 컨트롤러 상품 저장 로직 추가

--- spring-mvc2/file_practice/src/main/java/hello/file_practice/controller/ItemController.java
+++ spring-mvc2/file_practice/src/main/java/hello/file_practice/controller/ItemController.java
@@ -1,16 +1,40 @@
 package hello.file_practice.controller;
 
+import hello.file_practice.domain.Item;
+import hello.file_practice.domain.ItemRepository;
+import hello.file_practice.domain.UploadFile;
+import hello.file_practice.service.FileStore;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class ItemController {
-
-    @GetMapping("items/new")
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore ;
+    @GetMapping("/items/new")
     public String newForm(@ModelAttribute ItemForm itemForm){
         return "item-form";
+    }
+    @PostMapping("/items/new")
+    public String save(@ModelAttribute ItemForm itemForm, RedirectAttributes redirectAttributes){
+        UploadFile uploadFile = fileStore.storeFile(itemForm.getAttachFile());
+        List<UploadFile> uploadFiles = fileStore.storeFiles(itemForm.getImageFiles());
+        Item item = new Item();
+        item.setItemName(itemForm.getItemName());
+        item.setAttachFile(uploadFile);
+        item.setImageFiles(uploadFiles);
+
+        itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", item.getId());
+        return "redirect:/item/{itemId}";
     }
 }
