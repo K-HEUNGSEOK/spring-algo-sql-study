a5c9c36082a930ebfce73e2c8daf5c7bc5441365
김흥석 <eovnfjfpa@naver.com>
2025-12-04 22:28:40 +0900

    feat : file 예제 관련 스스로 만들어보는 폴더 추가

---
+++ spring-mvc2/file_practice/src/main/java/hello/file_practice/FilePracticeApplication.java
@@ -0,0 +1,13 @@
+package hello.file_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilePracticeApplication.class, args);
+	}
+
+}
