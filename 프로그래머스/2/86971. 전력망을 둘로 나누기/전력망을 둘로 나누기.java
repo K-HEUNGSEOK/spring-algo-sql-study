c3e2c0508af62d5941526141ce5e2d756cfed3eb
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-12-11 20:50:10 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.53 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,47 +1,48 @@
 import java.util.*;
 class Solution {
-    static List<Integer>[] graph;
+    static List<Integer> graph[];
     static boolean[] ch;
-    static int ans, result;
     public int solution(int n, int[][] wires) {
+        //1.먼저 리스트에 다 등록해둔다. (입력값을 다 준비한다)
         graph = new ArrayList[n+1];
         for(int i = 0 ; i <= n ; i++){
             graph[i] = new ArrayList<Integer>();
         }
         
-        for(int[] temp : wires){
-            int a = temp[0];
-            int b = temp[1];
+        for(int[] number : wires){
+            int a = number[0];
+            int b = number[1];
+            
             graph[a].add(b);
             graph[b].add(a);
         }
-        result = Integer.MAX_VALUE;
-        for(int[] temp : wires){
-            int a = temp[0];
-            int b = temp[1];
+        int result = Integer.MAX_VALUE;
+        
+        //2.그래프에서 한개씩 빼보면서 DFS를 돌리고 결과값을 계산한다.
+        for(int[] number : wires){
+            int a = number[0];
+            int b = number[1];
             
             graph[a].remove((Object) b);
             graph[b].remove((Object) a);
             
             ch = new boolean[n+1];
-            ans = DFS(a);
+            int ans = DFS(a);
             result = Math.min(result, Math.abs(ans - (n-ans)));
-   
+            
             graph[a].add(b);
             graph[b].add(a);
         }
         return result;
-        
     }
-    
     static int DFS(int v){
-        int count = 1;
-        ch[v]=true;
+        int cnt = 1;
+        ch[v] = true;
         for(int num : graph[v]){
             if(!ch[num]){
-                count += DFS(num);
+                cnt += DFS(num);
             }
         }
-        return count;
+        return cnt;
     }
 }
